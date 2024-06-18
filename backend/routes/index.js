901fa36ea1e7941cd0c751331bd5763e1e922364
shanda <brucewang147862@gmail.com>
2024-06-18 00:10:40 -0700

    phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,12 @@
 // backend/routes/index.js
 const express = require("express");
 const router = express.Router();
+// backend/routes/index.js
+// ...
+const apiRouter = require("./api");
+
+router.use("/api", apiRouter);
+// ...
 
 // backend/routes/index.js
 // ...
