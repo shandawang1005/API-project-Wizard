85ce82d85af4e0637549ba46f64d82a53784bc86
shanda <brucewang147862@gmail.com>
2024-06-20 18:03:04 -0700

    updated

--- backend/db/migrations/20240620211824-create-spot.js
+++ backend/db/migrations/20240620211824-create-spot.js
@@ -16,7 +16,7 @@ module.exports = {
       },
       ownerId: {
         type: Sequelize.INTEGER,
-        references: { model: "Users" },
+        references: { model: options.tableName },
         allowNull: false,
       },
       address: {
